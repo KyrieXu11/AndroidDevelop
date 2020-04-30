bd92192b0cd1b10c336cf2b986a38c8d37b8df0f
KyrieXu <kyriexu11@126.com>
2020-04-30 09:35:20 +0800

    修改了作业4

--- HomeWork04/app/src/main/java/com/kyriexu/homework04/fragment/Tab03Fragment.java
+++ HomeWork04/app/src/main/java/com/kyriexu/homework04/fragment/Tab03Fragment.java
@@ -1,15 +1,20 @@
 package com.kyriexu.homework04.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.kyriexu.homework04.R;
 import com.kyriexu.homework04.adapter.Tab03Adapter;
 import com.kyriexu.homework04.db.SQLUtil;
@@ -31,16 +36,50 @@ public class Tab03Fragment extends Fragment {
                              Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         // 获取上下文
-        FragmentActivity context = getActivity();
-        SQLUtil util = new SQLUtil(context);
+        final FragmentActivity context = getActivity();
+        final SQLUtil util = new SQLUtil(context);
         // 查询所有数据
-        List<Friend> friends = util.getAllFriends();
-        Tab03Adapter adapter = new Tab03Adapter(context, friends);
+
 
         View view = inflater.inflate(R.layout.tab03, container, false);
 
-        RecyclerView recyclerView = view.findViewById(R.id.rcy_contact);
 
+        FloatingActionButton btn = view.findViewById(R.id.btn_add);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 弹出框
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                // 设置标题
+                builder.setTitle("请输入名称");
+                // 输入框
+                final EditText edt_txt = new EditText(context);
+                // 创建poJo
+                final Friend friend = new Friend();
+                // 设置编辑框
+                builder.setView(edt_txt);
+                // 确定按钮点击事件
+                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String name = edt_txt.getText().toString();
+                        friend.setName(name);
+                        boolean b = util.insertFriend(friend);
+                        if (b)
+                            Toast.makeText(context, "添加成功", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(context, "添加失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("取消", null);
+                // 展示
+                builder.show();
+            }
+        });
+        // 查询所有的
+        List<Friend> friends = util.getAllFriends();
+        Tab03Adapter adapter = new Tab03Adapter(context, friends);
+        RecyclerView recyclerView = view.findViewById(R.id.rcy_contact);
         LinearLayoutManager layoutManager = new LinearLayoutManager(context,
                 LinearLayoutManager.VERTICAL, false);
         // set layout manager
--- HomeWork04/app/src/main/java/com/kyriexu/homework04/pojo/Friend.java
+++ HomeWork04/app/src/main/java/com/kyriexu/homework04/pojo/Friend.java
@@ -5,6 +5,10 @@ public class Friend {
 
     private String name;
 
+    public Friend(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "Friend{" +
